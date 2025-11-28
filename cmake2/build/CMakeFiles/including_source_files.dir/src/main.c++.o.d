8c218a96d221b30aa05b37bd8a9fce75aec3ebb3
Himanshu Dutt <dr.himanshudutt@gmail.com>
2025-11-28 15:30:43 +0530

    Delete cmake2/build directory

--- cmake2/build/CMakeFiles/including_source_files.dir/src/main.c++.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/including_source_files.dir/src/main.c++.o: \
- /workspaces/c-plus-plus-notes/cmake2/src/main.c++ \
- /usr/include/stdc-predef.h \
- /workspaces/c-plus-plus-notes/cmake2/include/lib.h
